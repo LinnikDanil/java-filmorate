bac8feb96233debefc43c8930f967f104ad9bf5d
Danil Linnik <danil.linn2015@yandex.ru>
2023-03-30 18:17:49 +0300

    fix: исправлены уточнения

--- src/main/java/ru/yandex/practicum/filmorate/model/User.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -2,7 +2,6 @@ package ru.yandex.practicum.filmorate.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NonNull;
 
 import javax.validation.constraints.*;
 import java.time.LocalDate;
